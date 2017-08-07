439c64f50a77fa04434cab3e565d76bedd7dc6b7
Nick Tsai <myintaer@gmail.com>
2017-08-07 09:55:14 +0800

    Update

---
+++ helpers/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+* 
+*/
+class Test
+{
+    
+    function __construct()
+    {
+        echo 'init';
+    }
+
+    public function run($value='')
+    {
+        echo 'run';
+    }
+}
